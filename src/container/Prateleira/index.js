64e534378c0918ed804c21b0e3d031fdd9af9546
gmbarroso <guilherme.barroso@pagar.me>
2019-04-08 18:49:54 -0300

    components: add Prateleira

---
+++ src/container/Prateleira/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SlideShow from '../../component/Slideshow/index'
+
+import './style.css'
+
+const Prateleira = () => (
+  <div className="slide">
+    <p align="left" className="title">Peças</p>
+      <SlideShow />
+  </div>
+)
+
+export default Prateleira
