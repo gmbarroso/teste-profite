f95c7b7ed5e9c08d36edf66e4a9bf8f66a98af30
gmbarroso <guilherme.barroso@pagar.me>
2019-04-08 18:48:29 -0300

    components: add Banner

---
+++ src/container/Banner/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import '../../index.css'
+import './style.css'
+
+const Banner = () => (
+  <section className="banner">
+    <div className="card-item textDiv">
+      Texto
+    </div>
+    <div className="card-item imageDiv">
+     Imagem
+    </div>
+  </section>
+)
+
+export default Banner
